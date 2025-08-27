34ad0f4661775a0fdd1e815ed108f410fa891144
AmanGupta1354 <amangupta1354@gmail.com>
2025-08-27 20:06:49 +0530

    upload

---
+++ src/first.jsx
@@ -0,0 +1,14 @@
+function first()
+{
+    return(
+        <>
+        <div className="card">
+            <h2>Aman Gupta</h2>
+            <p>Call: I1 ,Roll no :15 </p>
+        </div>
+        <br></br>
+        </>
+    );
+}
+
+export default first;
